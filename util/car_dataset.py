4ebbd848a23a59ca8dd0e30b8a35df90216992b6
Akshay Viswakumar <akshay.viswakumar@gmail.com>
2021-03-28 13:50:49 -0700

    Fix for Issue 12 (#13)
    #12 Resolved

--- util/car_dataset.py
+++ util/car_dataset.py
@@ -6,6 +6,7 @@ import sys
 import os
 import torch
 import pandas as pd
+import numpy as np
 from PIL import Image
 
 def check_dir(dir_path):
@@ -59,6 +60,10 @@ class CarDataset(torch.utils.data.Dataset):
             # If File Exists, Read into self.label_frames
             working_frame = pd.read_csv(label_file,sep=' ',names=['pitch','yaw'])
             #TODO: Dealing with NaN should happen around here
+            '''
+            Can't just drop NaN from the dataframe without reconciling
+            the image list.
+            '''
             self.label_frames.append(working_frame)
 
         # Run Validate To Catch any Error
@@ -111,13 +116,49 @@ class CarDataset(torch.utils.data.Dataset):
             len_in_folder = len(self.image_locations[folder_idx])
             if idx < len_in_folder:
                 # Required Index is Here!
-                sub_idx = idx - offset
-                return (folder_idx,sub_idx)
+                return (folder_idx,idx)
             offset+=len_in_folder
             idx -= offset
 
         raise DatasetError(("Index {} Out of Bounds in dataset of size"
                 " {}").format(idx,len(self)))
+
+    def has_nan(self,item):
+        '''
+        Returns True if item has a NaN value in angles.
+        '''
+        return any(torch.isnan(item['angles']))
+
+    def drop_nan(self):
+        '''
+        Labelframes may contain NaN values. Rows with NaN should be
+        dropped. Corresponding images should also be discarded.
+
+        Loops through dataset and drops items in the image list and
+        corresponding dataframe.
+        '''
+        new_image_locations = list()
+        new_label_frames = list()
+        for df in self.label_frames:
+            new_label_frames.append(df.iloc[0:0,:].copy())
+            new_image_locations.append(list())
+
+        # Start Loop
+        for ind in range(len(self)):
+            if self.has_nan(self[ind]):
+                continue
+            else:
+                folder_idx, sub_idx = self.get_offset(ind)
+                # Copy to New image list
+                loc = (self.image_locations[folder_idx][sub_idx])
+                new_image_locations[folder_idx].append(loc)
+                # Copy to New Dataframe
+                val = self.label_frames[folder_idx].iloc[sub_idx]
+                new_df = new_label_frames[folder_idx].append(val)
+                new_label_frames[folder_idx] = new_df
+
+        self.image_locations = new_image_locations
+        self.label_frames = new_label_frames
 
 class DatasetError(Exception):
     def __init__(self,message="Problem loading data!"):
